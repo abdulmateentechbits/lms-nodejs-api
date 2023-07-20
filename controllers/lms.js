2e5062d77b24efa7135972b2905f46b479adda63
abdulmateentechbits <mateen@techibits.com>
2023-07-20 15:43:34 +0500

    Add mocked data for courses and implemet auth method

--- controllers/lms.js
+++ controllers/lms.js
@@ -1,22 +1,36 @@
 import express from 'express';
-
+import { subCategories } from '../utils/courses.js';
 
 const router = express.Router();
 export const doLogin = async (req, res) => {
+    const {name, password} = req.body;
 
-    res.status(200).json("doLogin");
+    const option = {
+        method:'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            username: name,
+            password: password,
+            expiresInMins: 60, // optional
+          })
+    }
+    
+    try {
+        
+          fetch('https://dummyjson.com/auth/login',option).
+          then(res=>res.json()).
+          then((result)=>{
+            console.log("result: ",result);
+            res.status(200).json(result)
+          })
+        
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
 }
 
 export const getCourses = async (req, res) => {
-    res.status(200).json("getCourses");
-}
-
-export const getCourse = async (req, res) => {
-    res.status(200).json("getCourse");
-}
-
-export const doLogout = async (req, res) => {
-    res.status(200).json("doLogout");
+    res.status(200).json({data:subCategories, message:'Success'});
 }
 
 export const deleteCourse = async (req, res) => {
@@ -25,5 +39,13 @@ export const deleteCourse = async (req, res) => {
 export const updateCourse = async (req, res) => {
     res.status(200).json("updateCourse");
 }
+export const getCourse = async (req, res) => {
+    res.status(200).json("getCourse");
+}
+
+export const doLogout = async (req, res) => {
+    res.status(200).json("doLogout");
+}
+
 
 export default router;
